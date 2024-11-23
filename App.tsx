5e2908656e9b1ed116bf10e20ed42328adc7d7b1
Shiva <techdroidv5@gmail.com>
2024-11-23 11:21:23 +0530

    Change 3

--- App.tsx
+++ App.tsx
@@ -15,7 +15,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#f0f8ff', // Light blue background
+    backgroundColor: '#f0f8ff', 
     padding: 20,
   },
   heading: {
